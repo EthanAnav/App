f7e110fd15869521525d63c769acf120cccc2ae5
Ethan Anav <70921451+EthanAnav@users.noreply.github.com>
2022-11-26 10:05:30 -0800

    Update IndividualChat.java

--- IndividualChat.java
+++ IndividualChat.java
@@ -1,15 +1,27 @@
+import java.util.Scanner;
+
 public class IndividualChat {
 	private ContactList Contact;
 	private Message Message;
 	ChatHistory Chat;
 	
 	public void getMessage() {
-		for (int i = 0; i < Message.content.length; i++) {
-			System.out.println(Message.content[i]);
-		}
+		System.out.println(Message.getText());
 	}
 	
-	public void setMessage() {}
+	public void setMessage() {
+		Scanner in = new Scannner(System.in);
+		System.out.println("Enter message type: ");
+		String type = in.nextLine();
+		Message.setType(type);
+		System.out.println("Enter message status: ");
+		String status = in.nextLine();
+		Message.setStatus(status);
+		System.out.println("Enter message text: ");
+		String text = in.nextLine();
+		Message.setText(text);
+		in.close();
+	}
 	
 	public void getNotification() {}
 }
