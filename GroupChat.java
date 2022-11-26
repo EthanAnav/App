e0f6d543f01317317a8cf83d6bca9a618dfa258b
Ethan Anav <70921451+EthanAnav@users.noreply.github.com>
2022-11-26 10:11:51 -0800

    Update GroupChat.java

--- GroupChat.java
+++ GroupChat.java
@@ -1,12 +1,28 @@
+import java.util.Scanner
+
 public class GroupChat {
 	public String groupID;
 	public String groupName;
 	private Message Message;
 	private ChatHistory Chat;
 	
-	public void getMessage() {}
+	public void getMessage() {
+		System.out.println(Message.getText());
+	}
 	
-	public void setMessage() {}
+	public void setMessage() {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter message type: ");
+		String type = in.nextLine();
+		Message.setType(type);
+		System.out.println("Enter message status: ");
+		String status = in.nextLine();
+		message.setStatus(status);
+		System.out.println("Enter message text: ");
+		String text = in.nextLine();
+		Message.setText(text);
+		in.close();
+	}
 	
 	public boolean login() {
 		return false;
